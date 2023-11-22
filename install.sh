a204d9057d1fee9c7f133551ffeb8bfbad1e6385
Felix Salcher <dev.felix.salcher@gmail.com>
2023-11-22 19:03:55 +0100

    added kitty

--- install.sh
+++ install.sh
@@ -14,5 +14,13 @@ sudo $pckmng install node php php-dom php-curl php-sqlite3 composer
 
 # Bun
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
-     
+
+# Kitty
+curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin
+ln -sf ~/.local/kitty.app/bin/kitty ~/.local/kitty.app/bin/kitten ~/.local/bin/
+cp ~/.local/kitty.app/share/applications/kitty.desktop ~/.local/share/applications/
+sed -i "s|Icon=kitty|Icon=/home/$USER/.local/kitty.app/share/icons/hicolor/256x256/apps/kitty.png|g" ~/.local/share/applications/kitty*.desktop
+sed -i "s|Exec=kitty|Exec=/home/$USER/.local/kitty.app/bin/kitty|g" ~/.local/share/applications/kitty*.desktop
+ln -sf $PWD/kitty/kitty.conf ~/.config/kitty/kitty.conf
+
 ln -sf $PWD/.ideavimrc ~/.ideavimrc
