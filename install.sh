388332062613aa26a4c73af6121934f61e8472dd
Felix Salcher <dev.felix.salcher@gmail.com>
2023-11-24 10:48:39 +0100

    modified dotfiles in order to work properly

--- install.sh
+++ install.sh
@@ -2,15 +2,25 @@ mkdir -p ~/.local/bin
 
 pckmng=${1:-"apt"}
 
-sudo $pckmng install zsh
+sudo $pckmng install zsh vim curl git
 
 ln -sf $PWD/zsh/.zshrc ~/.zshrc 
 ln -sf $PWD/zsh/.p10k.zsh ~/.p10k.zsh
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
 sudo $pckmng install node php php-dom php-curl php-sqlite3 composer 
+
+# Brave
+sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+sudo $pckmng update
+sudo $pckmng install brave-browser
 
 # Bun
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
@@ -22,5 +32,8 @@ cp ~/.local/kitty.app/share/applications/kitty.desktop ~/.local/share/applicatio
 sed -i "s|Icon=kitty|Icon=/home/$USER/.local/kitty.app/share/icons/hicolor/256x256/apps/kitty.png|g" ~/.local/share/applications/kitty*.desktop
 sed -i "s|Exec=kitty|Exec=/home/$USER/.local/kitty.app/bin/kitty|g" ~/.local/share/applications/kitty*.desktop
 ln -sf $PWD/kitty/kitty.conf ~/.config/kitty/kitty.conf
+ln -sf $PWD/kitty/current-theme.conf ~/.config/kitty/current-theme.conf
 
 ln -sf $PWD/.ideavimrc ~/.ideavimrc
+
+cp fonts/* /usr/share/fonts
